4af6ef1aef5c7350a2fffa86390b8010485e5715
rainf0rest <405763281@qq.com>
2016-10-15 22:45:55 +0800

    add the game UI frame

---
+++ app/src/main/java/com/example/rain/chickenfarmtycoon/GameActivity.java
@@ -0,0 +1,20 @@
+package com.example.rain.chickenfarmtycoon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by rain on 2016/10/15.
+ */
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.farm);
+
+
+    }
+
+}
